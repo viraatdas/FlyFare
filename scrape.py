c6508b96de53dbe18541d9cf40059d1f2dfa1431
Humbl3Stud3nt <waratjam@gmail.com>
2018-11-10 09:19:54 -0500

    Made skeleton more specific for generateLink()

--- scrape.py
+++ scrape.py
@@ -11,6 +11,9 @@ Travelocity
 scrape():
 get data from site given site specification and url link
 """
+
+SITES = ("kayak", "trivago", "travelocity", "tripadvisor")
+
 def generateLink(site, departFrom, arriveAt, departureDate):
     """
     params:
@@ -23,8 +26,24 @@ def generateLink(site, departFrom, arriveAt, departureDate):
     returns:
     str -> link to specified site with given information, sorted by price
     """
-
-    pass
+    try: 
+        assert site in SITES
+    except:
+        #TODO: Implement error catching
+        pass
+    else:
+        if site == SITES[0]:
+            # Kayak
+            pass
+        elif site == SITES[1]:
+            # trivago
+            pass
+        elif site == SITES[2]:
+            # travelocity
+            pass
+        elif site == SITES[3]:
+            # tripadvisor
+            pass
 
 def scrape(site, url):
     pass
