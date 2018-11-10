5e7c637bc771e2dfce6986fc7788c2820b25bbf7
Humbl3Stud3nt <waratjam@gmail.com>
2018-11-09 23:45:41 -0500

    Initial Commit; Added skeleton of scrape.py

---
+++ scrape.py
@@ -0,0 +1,18 @@
+def generateLink(site, departFrom, arriveAt, departureDate):
+    """
+    params:
+    site: str -> specifies which site is being scraped, allows for determination of how link is generated
+    
+    departFrom: str -> specifies airport to depart from
+    arriveAt  : str -> specifies airport to arrive at
+    departureDate: str in yyyy-mm-dd format -> specifies date of departure
+
+    returns:
+    str -> link to specified site with given information, sorted by price
+    """
+
+    pass
+
+def scrape(site):
+    pass
+
