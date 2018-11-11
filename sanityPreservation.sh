1d2adccaa34fbe00e9eae7d5091467829abc542a
Humbl3Stud3nt <waratjam@gmail.com>
2018-11-10 19:15:38 -0500

    Added script that allows for automatic creation of skill_env_package.zip

---
+++ sanityPreservation.sh
@@ -0,0 +1,11 @@
+mkdir skill_env_package
+pip install -r py/requirements.txt -t skill_env_package
+cp -r lambda/py/* skill_env_package/
+
+# zip everything 
+cd skill_env_package
+zip -r skill_env_package.zip *
+
+mv skill_env_package.zip ../skill_env_package.zip
+
+cd ..
